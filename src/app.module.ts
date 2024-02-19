5208a4e9aefb954b0df2e8a7297e91eef0e232b7
alexander <alexander.apricot@gmail.com>
2024-02-19 18:57:04 +0100

    Add CacheModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ValidationOptions, ValidationSchema } from './config/validation';
 import configuration from './config/configuration';
@@ -13,6 +15,20 @@ import configuration from './config/configuration';
       validationSchema: ValidationSchema,
       validationOptions: ValidationOptions,
       load: [configuration],
+    }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const host = configService.getOrThrow<string>('redis.host');
+        const port = configService.getOrThrow<number>('redis.port');
+        const password = configService.getOrThrow<string>('redis.password');
+        return {
+          isGlobal: true,
+          store: redisStore,
+          url: `redis://:${password}@${host}:${port}`,
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
