74ee5cbed2dd83c045869dfec26210c106d7a788
alexander <alexander.apricot@gmail.com>
2024-02-20 21:08:56 +0100

    Add @IsNotEqualTo validation decorator

---
+++ src/common/decorators/is-not-equal-to.ts
@@ -0,0 +1,26 @@
+import { registerDecorator, ValidationArguments } from 'class-validator';
+import { ValidationOptions } from 'joi';
+
+export function IsNotEqualTo(
+  property: string,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'isNotEqualTo',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [property],
+      options: validationOptions,
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          const targetValue = (args.object as any)[args.constraints[0]]; // Get the value of the target field
+          return value !== targetValue;
+        },
+        defaultMessage(args: ValidationArguments) {
+          return `${args.property} must be different from ${args.constraints[0]}.`;
+        },
+      },
+    });
+  };
+}
