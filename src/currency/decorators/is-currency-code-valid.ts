e769bcc28c394d32f4c051e4b9f0163c6836941e
alexander <alexander.apricot@gmail.com>
2024-02-20 21:10:09 +0100

    Add @IsCurrencyCodeValid validation decorator

---
+++ src/currency/decorators/is-currency-code-valid.ts
@@ -0,0 +1,37 @@
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+import { ValidationOptions } from 'joi';
+import {
+  CurrencyCode,
+  currencyCodeToIsoCode,
+} from '../types/currency-code.type';
+
+@ValidatorConstraint({ async: false })
+export class IsCurrencyCodeValidConstraint
+  implements ValidatorConstraintInterface
+{
+  validate(currencyCode: CurrencyCode, args: ValidationArguments) {
+    return currencyCodeToIsoCode.hasOwnProperty(currencyCode);
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    // todo append a link to swagger docs with a complete list of supported currency codes
+    return `${args.property} is not a valid currency code. Valid codes example: UAH, USD etc.`;
+  }
+}
+
+export function IsCurrencyCodeValid(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsCurrencyCodeValidConstraint,
+    });
+  };
+}
