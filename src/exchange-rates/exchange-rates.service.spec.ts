ca309ca943675d56e22ea74a1758854d6363e03d
alexander <alexander.apricot@gmail.com>
2024-02-21 10:05:26 +0100

    Fix exchange-rates.service.spec.ts

--- src/exchange-rates/exchange-rates.service.spec.ts
+++ src/exchange-rates/exchange-rates.service.spec.ts
@@ -1,12 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExchangeRatesService } from './exchange-rates.service';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 describe('ExchangeRatesService', () => {
   let service: ExchangeRatesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExchangeRatesService],
+      providers: [
+        ExchangeRatesService,
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn(),
+            post: jest.fn(),
+            put: jest.fn(),
+          },
+        },
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn().mockImplementation((key) => `${key}-value-from-config`),
+          },
+        },
+        {
+          provide: CACHE_MANAGER,
+          useValue: {
+            get: jest.fn(),
+            set: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<ExchangeRatesService>(ExchangeRatesService);
@@ -15,4 +41,6 @@ describe('ExchangeRatesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // todo add more tests
 });
