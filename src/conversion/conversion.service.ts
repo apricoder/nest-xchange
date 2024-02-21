95e952d590015c1e0c66bc8c08ddc6088ecd94b7
alexander <alexander.apricot@gmail.com>
2024-02-21 01:09:12 +0100

    Reuse conversion logic for both cached / downloaded rates

--- src/conversion/conversion.service.ts
+++ src/conversion/conversion.service.ts
@@ -2,7 +2,7 @@ import * as _ from 'lodash';
 import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { ExchangeRatesService } from '../exchange-rates/exchange-rates.service';
 import { CurrencyCode } from '../currency/types/currency-code.type';
-import { ExchangeRate } from '../exchange-rates/types/exchange-rate.type';
+import { ExchangeRate, ExchangeRatesMap } from '../exchange-rates/types/exchange-rate.type';
 
 @Injectable()
 export class ConversionService {
@@ -11,53 +11,83 @@ export class ConversionService {
   constructor(private readonly ratesService: ExchangeRatesService) {}
 
   async convert(srcCurrency: CurrencyCode, tgtCurrency: CurrencyCode, amount: number) {
-    // first try to convert based on cached rate
-    const tgtAmountUsingCachedRate = await this.convertUsingCachedRateOrNull(srcCurrency, tgtCurrency, amount);
-
+    // first try to convert based on cached rates
+    const tgtAmountUsingCachedRate = await this.convertUsingCachedRate(srcCurrency, tgtCurrency, amount);
     if (tgtAmountUsingCachedRate) {
       return tgtAmountUsingCachedRate;
     }
 
-    // if couldn't convert with cached rate
-    // fetch fresh rates map from monobank
-    // if direct rate - calculate & return target amount
-    // else try to convert source -> uah -> target
-    // else give up
+    const { exchangeRatesMap, tgtAmount } = await this.convertUsingLatestFetchedRates(srcCurrency, tgtCurrency, amount);
+
+    // since latest rates are just downloaded - use them to update our cache
+    // no need for await to make user wait
+    this.ratesService.refreshExchangeRatesCache({ exchangeRatesMap, silentLog: true });
+
+    return tgtAmount;
   }
 
-  private async convertUsingCachedRateOrNull(
+  private async convertUsingLatestFetchedRates(
+    srcCurrency: CurrencyCode,
+    tgtCurrency: CurrencyCode,
+    amount: number,
+  ): Promise<{ exchangeRatesMap: ExchangeRatesMap; tgtAmount: number }> {
+    try {
+      const exchangeRatesMap = await this.ratesService.fetchLatestExchangeRatesMap();
+      const tgtAmount = await this.convertUsingRateProvider(
+        srcCurrency,
+        tgtCurrency,
+        amount,
+        async (srcCurrency, tgtCurrency) => {
+          const key = this.ratesService.getCurrencyPairKey(srcCurrency, tgtCurrency);
+          return exchangeRatesMap[key];
+        },
+      );
+      return { exchangeRatesMap, tgtAmount };
+    } catch (e) {
+      throw new InternalServerErrorException(`Error converting using freshly fetched rates: ${e}`);
+    }
+  }
+
+  private async convertUsingCachedRate(
     srcCurrency: CurrencyCode,
     tgtCurrency: CurrencyCode,
     amount: number,
   ): Promise<number | null> {
     try {
-      const directRate = await this.ratesService.getCachedExchangeRate(srcCurrency, tgtCurrency);
-      if (directRate) {
-        return this.calculateTargetAmount(amount, srcCurrency, tgtCurrency, directRate);
-      }
-
-      // if no direct rate & neither source nor target is uah
-      const noneIsUah = srcCurrency !== 'UAH' && tgtCurrency !== 'UAH';
-      if (noneIsUah) {
-        // get rates for indirect conversion like source -> uah -> target
-        const [srcUahRate, tgtUahRate] = await Promise.all([
-          this.ratesService.getCachedExchangeRate(srcCurrency, 'UAH'),
-          this.ratesService.getCachedExchangeRate(tgtCurrency, 'UAH'),
-        ]);
-
-        if (!srcUahRate || !tgtUahRate) {
-          return null;
-        }
-
-        // 2-step conversion: source -> uah, uah -> target
-        const uahAmount = this.calculateTargetAmount(amount, srcCurrency, 'UAH', srcUahRate);
-        const tgtAmount = this.calculateTargetAmount(uahAmount, 'UAH', tgtCurrency, tgtUahRate);
-
-        return tgtAmount;
-      }
+      return this.convertUsingRateProvider(srcCurrency, tgtCurrency, amount, this.ratesService.getCachedExchangeRate);
     } catch (e) {
       this.logger.error(`Error converting using cached rate: ${e}`);
       return null;
+    }
+  }
+
+  private async convertUsingRateProvider(
+    srcCurrency: CurrencyCode,
+    tgtCurrency: CurrencyCode,
+    amount: number,
+    // Either redis cache or latest downloaded exchange rates
+    provideRate: (srcCurrency: CurrencyCode, tgtCurrency: CurrencyCode) => Promise<ExchangeRate>,
+  ): Promise<number | null> {
+    const directRate = await provideRate(srcCurrency, tgtCurrency);
+    if (directRate) {
+      return this.calculateTargetAmount(amount, srcCurrency, tgtCurrency, directRate);
+    }
+
+    // if no direct rate & neither source nor target is uah
+    const noneIsUah = srcCurrency !== 'UAH' && tgtCurrency !== 'UAH';
+    if (noneIsUah) {
+      // get rates for indirect conversion like source -> uah -> target
+      const [srcUahRate, tgtUahRate] = await Promise.all([provideRate(srcCurrency, 'UAH'), provideRate(tgtCurrency, 'UAH')]);
+
+      if (!srcUahRate || !tgtUahRate) {
+        return null;
+      }
+
+      // 2-step conversion: source -> uah, uah -> target
+      const uahAmount = this.calculateTargetAmount(amount, srcCurrency, 'UAH', srcUahRate);
+      const tgtAmount = this.calculateTargetAmount(uahAmount, 'UAH', tgtCurrency, tgtUahRate);
+
+      return tgtAmount;
     }
   }
 
