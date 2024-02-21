ea2dbe656c35f9080fa7405478bce77022ed6a83
alexander <alexander.apricot@gmail.com>
2024-02-21 12:26:03 +0100

    Drop unused app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ValidationOptions, ValidationSchema } from './config/validation';
 import configuration from './config/configuration';
 import { ExchangeRatesModule } from './exchange-rates/exchange-rates.module';
@@ -24,6 +23,5 @@ import { ConversionModule } from './conversion/conversion.module';
     ConversionModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
